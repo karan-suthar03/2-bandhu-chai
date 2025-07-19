c3ae2697bd262d4b502baf43f5f27352b582a0f5
karan suthar <karan.suthar.lakha@gmail.com>
2025-07-19 15:30:06 +0530

    Refactor: Add navigation to Shop button in HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,11 @@
 import logo from "../assets/logo.svg";
+import {useNavigate} from "react-router-dom";
 
 function HeroSection() {
+    const navigate = useNavigate();
+    function goToShop(){
+        navigate(`/shop`);
+    }
     return (
         <section className="flex flex-col-reverse md:flex-row items-center justify-between px-6 md:px-40 py-12 pt-32 md:min-h-[calc(100vh-100px)] relative overflow-hidden">
             <div className="bg-gradient-to-r from-[#f7ebc9] via-[#e8d5a3] to-[#f7ebc9] absolute inset-0 z-[-1] opacity-50" />
@@ -13,7 +18,9 @@ function HeroSection() {
                     रिश्तों की मिठास, चाय के साथ ख़ास
                 </p>
 
-                <button className="bg-[#a0522d] text-white px-6 py-3 rounded-full text-lg hover:bg-[#8b4513] transition">
+                <button className="bg-[#a0522d] text-white px-6 py-3 rounded-full text-lg hover:bg-[#8b4513] transition cursor-pointer"
+                    onClick={goToShop}
+                >
                     Shop Now
                 </button>
             </div>
